99e8b7aa87486e46b2fe0df42b74179d31b90b5c
YanCong0708 <48944989+YanCong0708@users.noreply.github.com>
2019-07-18 14:08:33 +0800

    Update EuekaDemoApplicationTests.java
    加个注释

--- eueka-demo/src/test/java/com/soft863/euekademo/EuekaDemoApplicationTests.java
+++ eueka-demo/src/test/java/com/soft863/euekademo/EuekaDemoApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class EuekaDemoApplicationTests {
-
+//加个注释
     @Test
     public void contextLoads() {
     }
